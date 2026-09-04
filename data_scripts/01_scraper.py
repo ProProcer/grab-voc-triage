1a677f49d1af4814afea4bec0954db6e9e4e9936
Matthew Alexander P <39817115+ProProcer@users.noreply.github.com>
2026-09-04 11:41:04 +0700

    script to scrape the dataset

---
+++ data_scripts/01_scraper.py
@@ -0,0 +1,16 @@
+from google_play_scraper import Sort, reviews
+import pandas as pd
+
+app_id = 'com.grabtaxi.passenger'
+
+result, _ = reviews(
+    app_id,
+    lang='id',              # 'id' for Indonesian, 'ms' for Malay
+    country='id',           # 'id' or 'my'
+    sort=Sort.NEWEST,
+    count=10000,            # Pull 10k reviews
+    filter_score_with=None  # Or pass 1, 2, 3 to grab low ratings directly
+)
+
+df = pd.DataFrame(result)[['userName', 'score', 'at', 'content']]
+df.to_csv('data/raw/grab_raw_reviews.csv', index=False)  
