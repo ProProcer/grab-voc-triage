2510f8018c9184ec0129ba968e9271c3fa973fe0
Matthew Alexander P <39817115+ProProcer@users.noreply.github.com>
2026-09-07 16:43:43 +0700

    add the manual annoatation export and conversion args

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 APP_ID = 'com.grabtaxi.passenger'
 RAW_DATA_PATH = 'data/raw/grab_reviews.csv'
 CLEANED_DATA_PATH = 'data/processed/grab_reviews.csv'
+MANUAL_ANNOTATION_TEMPLATE_DATA_PATH = 'data/processed/manual_annotation_template.xlsx'
 MANUAL_ANNOTATION_DATA_PATH = 'data/processed/manual_annotation.csv'
+MANUAL_ANNOTATION_EXCEL_DATA_PATH = 'data/processed/manual_annotation.xlsx'
 RANDOM_STATE = 12
--- data_scripts/03_manual_annotation.py
+++ data_scripts/03_manual_annotation.py
@@ -1,6 +1,24 @@
 import config
 import pandas as pd
+from pathlib import Path
+import argparse
 
-df = pd.read_csv(config.CLEANED_DATA_PATH)
-selected = df.groupby('score').sample(frac = 50.2 / len(df), random_state = config.RANDOM_STATE)
-selected.to_csv(config.MANUAL_ANNOTATION_DATA_PATH)
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--force_template', action = 'store_true')
+    parser.add_argument('-e', '--force_excel2csv', action = 'store_true')
+    return parser.parse_args()
+
+def main():
+    args = parse_args()
+    if not Path(config.MANUAL_ANNOTATION_TEMPLATE_DATA_PATH).exists() or args.force_template:
+        df = pd.read_csv(config.CLEANED_DATA_PATH)
+        selected = df.groupby('score').sample(frac = 50.2 / len(df), random_state = config.RANDOM_STATE)
+        selected.to_excel(config.MANUAL_ANNOTATION_TEMPLATE_DATA_PATH)
+    if not Path(config.MANUAL_ANNOTATION_DATA_PATH).exists() or args.force_excel2csv:
+        df_annotated = pd.read_excel(config.MANUAL_ANNOTATION_EXCEL_DATA_PATH, index_col = 0)
+        df_annotated.index.name = None
+        df_annotated.to_csv(config.MANUAL_ANNOTATION_DATA_PATH)
+
+if __name__ == '__main__':
+    main()
