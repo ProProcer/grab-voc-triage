4e4d27a447b65261305e919d63c407430dfe53d2
Matthew Alexander P <39817115+ProProcer@users.noreply.github.com>
2026-09-09 21:45:42 +0700

    remove FULLFILLMENT FOOD and POS category

--- src/schemas/prediction.py
+++ src/schemas/prediction.py
@@ -3,7 +3,6 @@ from pydantic import BaseModel, create_model
 import config
 
 class SentimentLabel(str, Enum):
-    POS = "POS"
     NEG = "NEG"
     ABSENT = "ABSENT"
 
@@ -12,4 +11,3 @@ class ReviewClassification(BaseModel):
     DRIVER_OPS : SentimentLabel
     APP_AND_MAPS : SentimentLabel
     PRICING_AND_BILLING : SentimentLabel
-    FULFILLMENT_FOOD : SentimentLabel
