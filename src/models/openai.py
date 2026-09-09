3c27f31b681df569ac6e3dc69edf3beec421814c
Matthew Alexander P <39817115+ProProcer@users.noreply.github.com>
2026-09-09 23:05:53 +0700

    add retry if RateLimitError to openai

--- src/models/openai.py
+++ src/models/openai.py
@@ -1,13 +1,19 @@
 from src.models.base import ReviewClassificationModel
 from src.schemas.prediction import ReviewClassification
-from openai import OpenAI
+from openai import OpenAI, RateLimitError
 import config
+from tenacity import retry, wait_exponential, stop_after_attempt, retry_if_exception_type
 
 class OpenAIModel(ReviewClassificationModel):
     def __init__(self, system_prompt : str, model : str):
         self.system_prompt = system_prompt
         self.model = model
         self.client = OpenAI()
+    @retry(
+        retry=retry_if_exception_type(RateLimitError),
+        wait=wait_exponential(multiplier=1, min=1, max=60),
+        stop=stop_after_attempt(5),
+    )
     def classify(self, text) -> dict:
         try:
             response = self.client.beta.chat.completions.parse(
@@ -20,6 +26,8 @@ class OpenAIModel(ReviewClassificationModel):
                 response_format=ReviewClassification,
             )
             return response.choices[0].message.parsed.model_dump()
+        except RateLimitError:
+            raise  
         except Exception as e:
             print(f"Inference error on review: {e}")
             return {
