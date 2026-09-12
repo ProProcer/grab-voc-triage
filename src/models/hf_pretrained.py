29d9d5a53b2d8fd30c20880149a89b13243f34e7
Matthew Alexander P <39817115+ProProcer@users.noreply.github.com>
2026-09-12 22:00:07 +0700

    add the indobert model

---
+++ src/models/hf_pretrained.py
@@ -0,0 +1,35 @@
+from transformers import AutoModel
+from torch import nn
+import config
+
+class ReviewClassifier(nn.Module):
+    def __init__(self, pretrained_model : str, num_labels : int = len(config.CATEGORIES), dropout_prob = 0.3):
+        super().__init__()
+        self.encoder = AutoModel.from_pretrained(pretrained_model)
+        hidden_size = self.encoder.config.hidden_size
+        self.dropout = nn.Dropout(dropout_prob)
+        self.classifier = nn.Linear(hidden_size, num_labels)
+
+    def forward(self, input_ids, attention_mask):
+        outputs = self.encoder(input_ids = input_ids, attention_mask = attention_mask)
+
+        cls_output = (
+            outputs.pooler_output 
+            if hasattr(outputs, 'pooler_outputs') and 
+            outputs.pooler_output 
+            else outputs.last_hidden_state[:, 0, :]
+        )
+
+        retained = self.dropout(cls_output)
+        logits = self.classifier(retained)
+        return logits
+
+if __name__ == "__main__":
+    model = ReviewClassifier('models/indobert-base-p1')
+    from src.data.dataset import ReviewDataset
+    dataset = ReviewDataset('data/processed/test.parquet', pretrained_model='models/indobert-base-p1', max_len = 128)
+    data = dataset[0]
+    print(model(data['input_ids'].unsqueeze(0), data['attention_mask'].unsqueeze(0)))
+
+
+
